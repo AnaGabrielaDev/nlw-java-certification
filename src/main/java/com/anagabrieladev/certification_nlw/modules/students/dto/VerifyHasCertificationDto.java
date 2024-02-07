a5a6a253ae358df5ab72232557ce6ed3d5b51c28
AnaGabriela <anagabrieladev@gmail.com>
2024-02-07 14:11:42 -0300

    ✨ feat: Verify if has certification dto

---
+++ src/main/java/com/anagabrieladev/certification_nlw/modules/students/dto/VerifyHasCertificationDto.java
@@ -0,0 +1,14 @@
+package com.anagabrieladev.certification_nlw.modules.students.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class VerifyHasCertificationDto {
+    private String email;
+    private String technology;
+}
